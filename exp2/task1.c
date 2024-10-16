9ae91f48a6c7a7b536198734a5d2d4ef5af4a542
thehusain21 <husainsitamuwala@eng.rizvi.edu.in>
2024-10-16 10:26:04 +0530

    Create task1.c

---
+++ exp2/task1.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("*** EVEN ODD Finder ***\n");
+    printf("Enter Number=");
+    scanf("%d",&num);
+
+    if(num%2==0)
+
+    {printf("%d is an even",num);
+
+    }
+    else
+
+    {printf("%d is an odd",num);
+    }
+return 0;
+
+}
