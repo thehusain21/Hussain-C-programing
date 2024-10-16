377c3ba77c6d7e2171693fbab18bef35920bb3e7
thehusain21 <husainsitamuwala@eng.rizvi.edu.in>
2024-10-16 10:11:52 +0530

    Create task1.c

---
+++ exp1/task1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()  {
+    printf("name  \t:Hussain Sitamouwala \n");
+    printf("age \t:18\n" );
+    printf("class \t:ECS \n");
+    printf("div\t:E \n");
+    printf("UIN \t:241S033 \n");
+    return(0);
+}
